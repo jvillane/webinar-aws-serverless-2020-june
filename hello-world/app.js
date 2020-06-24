d3636df6c9b0b9c4e8efb4074eb4dbe872195d94
Francisco Casals <francisco.casals@gmail.com>
2020-06-24 11:46:52 -0400

    ADD: final code, demo 1 webinar

---
+++ hello-world/app.js
@@ -0,0 +1,36 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async(event, context) => {
+    try {
+        // const ret = await axios(url);
+        let name = event.name == undefined ? event.pathParameters.name : event.name;
+        console.log(name + ' está presente');
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello webinar!...' + name,
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
